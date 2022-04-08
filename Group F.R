aacc1c899574b0d3ec7d4711c008b72901420ca0
omm223 <98827277+omm223@users.noreply.github.com>
2022-04-07 20:05:17 -0400

    Add files via upload

---
+++ Group F.R
@@ -0,0 +1,13 @@
+#Import Data Sets
+library(readxl)
+Team_Offense <- read_excel("Desktop/Team Offense.xlsx")
+View(Team_Offense)
+
+library(readxl)
+NFC_Standings <- read_excel("Desktop/NFC Standings.xlsx")
+View(NFC_Standings)
+
+library(readxl)
+AFC_Standings <- read_excel("Desktop/AFC Standings.xlsx")
+View(AFC_Standings)
+
